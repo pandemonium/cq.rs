5394bfb35becda6fc1330a09c51ca20a7415de60
Patrik Andersson <pandersson@gmail.com>
2024-10-26 10:55:43 +0200

    Refactors and fleshes out some more of the queries.

--- api_client/src/main.rs
+++ api_client/src/main.rs
@@ -1,4 +1,6 @@
+use model::Book;
 use reqwest::Client;
+use serde::{de::DeserializeOwned, Deserialize};
 
 struct ApiClient {
     http_client: Client,
@@ -17,7 +19,7 @@ impl ApiClient {
         }
     }
 
-    fn resolve_uri(&self, resource_uri: &str) -> String {
+    fn resolve_resource_uri(&self, resource_uri: &str) -> String {
         format!(
             "{}{}{resource_uri}",
             self.base_url,
@@ -26,7 +28,36 @@ impl ApiClient {
     }
 
     pub async fn get_books(&self) -> error::Result<Vec<model::Book>> {
-        let resource_uri = self.resolve_uri("/books");
+        self.request_resource("/books").await
+    }
+
+    pub async fn get_authors(&self) -> error::Result<Vec<model::Author>> {
+        self.request_resource("/authors").await
+    }
+
+    pub async fn get_author_by_book(
+        &self,
+        book_id: model::BookId,
+    ) -> error::Result<Option<model::Author>> {
+        self.request_resource(&format!("/books/{}/author", book_id))
+            .await
+    }
+
+    pub async fn get_books_by_author(
+        &self,
+        author_id: model::AuthorId,
+    ) -> error::Result<Vec<model::Book>> {
+        self.request_resource(&format!("/authors/{}/books", author_id))
+            .await
+    }
+
+    // An ADT can be constructed around the Resource abstraction to deal
+    // with the ugly stringly typed mess of paths that it is currently
+    async fn request_resource<R>(&self, resource_uri: &str) -> error::Result<R>
+    where
+        R: DeserializeOwned,
+    {
+        let resource_uri = self.resolve_resource_uri(resource_uri);
         let request = self.http_client.get(resource_uri).build()?;
         let response = self.http_client.execute(request).await?;
         Ok(serde_json::from_slice(&response.bytes().await?)?)
@@ -49,20 +80,32 @@ mod error {
     pub type Result<A> = StdResult<A, Error>;
 }
 
+// Think about extracting his and the stuff in http::model
+// into a common crate "model types" or somesuch
 mod model {
+    use core::fmt;
+
     use serde::{Deserialize, Serialize};
     use uuid::Uuid;
 
-    #[derive(Serialize, Deserialize)]
-    pub struct Author;
+    #[derive(Debug, Serialize, Deserialize)]
+    pub struct Author {
+        pub id: AuthorId,
+        pub info: AuthorInfo,
+    }
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct AuthorId(pub Uuid);
 
     #[derive(Debug, Serialize, Deserialize)]
+    pub struct AuthorInfo {
+        pub name: String,
+    }
+
+    #[derive(Debug, Serialize, Deserialize)]
     pub struct Book {
-        id: BookId,
-        info: BookInfo,
+        pub id: BookId,
+        pub info: BookInfo,
     }
 
     #[derive(Debug, Serialize, Deserialize)]
@@ -70,16 +113,47 @@ mod model {
 
     #[derive(Debug, Serialize, Deserialize)]
     pub struct BookInfo {
-        isbn: String,
-        title: String,
-        author: AuthorId,
+        pub isbn: String,
+        pub title: String,
+        pub author: AuthorId,
+    }
+
+    impl fmt::Display for BookId {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let Self(id) = self;
+            write!(f, "{id}")
+        }
+    }
+
+    impl fmt::Display for AuthorId {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            let Self(id) = self;
+            write!(f, "{id}")
+        }
     }
 }
 
 #[tokio::main]
 async fn main() {
     let client = ApiClient::new("http://dsky.local:3000");
-    let books = client.get_books().await.expect("some books");
 
+    let books = client.get_books().await.expect("some books");
     println!("Books: {books:?}");
+
+    let authors = client.get_authors().await.expect("some authors");
+    println!("Authors: {authors:?}");
+
+    for Book { id, info } in books {
+        let author = client.get_author_by_book(id).await.expect("book's author");
+        println!(
+            "{} by {}",
+            info.title,
+            author.expect("existing author").info.name
+        );
+    }
+
+    for model::Author { id, info } in authors {
+        let books = client.get_books_by_author(id).await.expect("some books");
+        println!("Books: {books:?}");
+    }
 }
